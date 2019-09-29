af23198851970d36d2d5a95111aead104c65ca3c
xulinlin <18351942808@163.com>
2019-09-29 15:21:38 +0800

    使用postcss进行移动端适配

--- src/components/VFooter/VFooter.vue
+++ src/components/VFooter/VFooter.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="v-footer-wrapper"></div>
+  <div class="v-footer-wrapper">哈哈哈哈</div>
 </template>
 
 <style lang="scss" scoped>
 .v-footer-wrapper {
   background-color: aqua;
   height: 60px;
+  font-size: 28px;
 }
 </style>
